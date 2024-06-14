40e78940f9facfd85770a65622429ebbc95f53bc
comi190327 <comi@actails.com>
2024-06-14 23:47:23 +0900

    class-transformerまで完了

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
-import _ from "lodash";
+import "reflect-metadata";
+import { plainToInstance } from "class-transformer";
+import { Product } from "./product.model";
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+  { title: "商品1", price: 100 },
+  { title: "商品2", price: 200 },
+];
+
+// const p1 = new Product("商品1", 100);
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+// for (const prod of loadedProducts) {
+//   console.log(prod.getInformation());
+// }
+
+const loadedProducts = plainToInstance(Product, products);
