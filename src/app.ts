9035c8534573d962c80689cb6e9f5a355fe683ec
comi190327 <comi@actails.com>
2024-06-25 18:27:59 +0900

    サードパーティライブラリ＆TypeScript完了#1

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import "reflect-metadata";
 import { plainToInstance } from "class-transformer";
 import { Product } from "./product.model";
+import { validate } from "class-validator";
+import { errors } from "undici-types";
 
 const products = [
   { title: "商品1", price: 100 },
   { title: "商品2", price: 200 },
 ];
+
+const newProd = new Product("", -100);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("バリデーションエラー！");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 // const p1 = new Product("商品1", 100);
 
